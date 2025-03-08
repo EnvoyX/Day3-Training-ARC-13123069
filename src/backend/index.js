db51c4b2b664969c0558938da74e1e4a99b243e5
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-08 23:52:06 +0700

    fix : added some other line to ensure CORS working

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-const cors = require("cors");
 const bodyParser = require(`body-parser`);
 const db = require("./connection");
 const response = require("./response");
-app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors());
 app.options("*", cors());
+app.use(bodyParser.json());
 
 app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
